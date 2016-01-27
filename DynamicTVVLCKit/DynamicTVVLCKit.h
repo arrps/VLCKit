0d386d30ac01c5b54cab1cb724e31a9fdd6eac31
Aman Mishra <aman.rajjeev@gmail.com>
2016-01-27 06:23:16 -0800

    //Initial commit with dynamic VLC kit support for TV.

---
+++ DynamicTVVLCKit/DynamicTVVLCKit.h
@@ -0,0 +1,38 @@
+//
+//  DynamicTVVLCKit.h
+//  DynamicTVVLCKit
+//
+//  Created by Puru Shukla on 1/26/16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DynamicTVVLCKit.
+FOUNDATION_EXPORT double DynamicTVVLCKitVersionNumber;
+
+//! Project version string for DynamicTVVLCKit.
+FOUNDATION_EXPORT const unsigned char DynamicTVVLCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicTVVLCKit/PublicHeader.h>
+
+
+#import <DynamicTVVLCKit/VLCAudio.h>
+#import <DynamicTVVLCKit/VLCLibrary.h>
+#import <DynamicTVVLCKit/VLCMedia.h>
+#import <DynamicTVVLCKit/VLCMediaDiscoverer.h>
+#import <DynamicTVVLCKit/VLCMediaList.h>
+#import <DynamicTVVLCKit/VLCMediaPlayer.h>
+#import <DynamicTVVLCKit/VLCMediaListPlayer.h>
+#import <DynamicTVVLCKit/VLCMediaThumbnailer.h>
+#import <DynamicTVVLCKit/VLCTime.h>
+
+@class VLCMedia;
+@class VLCMediaLibrary;
+@class VLCMediaList;
+@class VLCTime;
+@class VLCVideoView;
+@class VLCAudio;
+@class VLCMediaThumbnailer;
+@class VLCMediaListPlayer;
+@class VLCMediaPlayer;
